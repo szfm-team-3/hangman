4e1a3d42f6fb8f1505d98efc92ef5faeade66d98
patrikKrekk <krekk.patrik@gmail.com>
2021-12-06 18:08:41 +0100

    kezdokepernyo es menu hozzaadasa

--- client.py
+++ client.py
@@ -19,49 +19,69 @@ def is_valid_letter(letter, data):
         return True
     return False
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    while True:
-        data = s.recv(1024).decode()
-        if not data:
-            print('Itt a vege')
-            break
-        data = data.split('|')
-        act_state = len(data[1])
-        if data[0] == 'VARAKOZAS':
-            if len(data) == 3:
-                redraw(act_state)
-                print()
-                print('Hibás betűk: ', data[1])
-                print('Szó:', data[2])
-                print('--- ELLENFÉL KÖRE ---')
-            else:
-                redraw(-1)
-                print('Várakozás a másik játékosra...')
-            continue
-        elif data[0] == 'MONDATBEKERES':
-            redraw(-1)
-            while True:
-                print('Adj meg egy mondatot.\nCsak betűkből és szóközökből, legalább 5, de maximum 20 karakterből állhat!')
-                word = input('> ')
-                word = ' '.join(word.split()).strip().upper()
-                if is_valid_word(word):
-                    s.sendall(word.encode())
-                    break;
+def start_game():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        while True:
+            data = s.recv(1024).decode()
+            if not data:
+                print('Itt a vege')
+                break
+            data = data.split('|')
+            act_state = len(data[1])
+            if data[0] == 'VARAKOZAS':
+                if len(data) == 3:
+                    redraw(act_state)
+                    print()
+                    print('Hibás betűk: ', data[1])
+                    print('Szó:', data[2])
+                    print('--- ELLENFÉL KÖRE ---')
                 else:
                     redraw(-1)
-                    print('Nem érvényes mondat!')
-        elif data[0] == 'BETUBEKERES':
-            redraw(act_state)
-            while True:
-                print('Hibás betűk: ', data[1])
-                print('Szó:', data[2])
-                print('Adj meg egy betűt!')
-                letter = input('> ')
-                letter = letter.strip().upper()
-                if is_valid_letter(letter, data):
-                    s.sendall(letter.encode())
-                    break
-                else:
-                    redraw(act_state)
-                    print('Nem érvényes betű!')
+                    print('Várakozás a másik játékosra...')
+                continue
+            elif data[0] == 'MONDATBEKERES':
+                redraw(-1)
+                while True:
+                    print('Adj meg egy mondatot.\nCsak betűkből és szóközökből, legalább 5, de maximum 20 karakterből állhat!')
+                    word = input('> ')
+                    word = ' '.join(word.split()).strip().upper()
+                    if is_valid_word(word):
+                        s.sendall(word.encode())
+                        break;
+                    else:
+                        redraw(-1)
+                        print('Nem érvényes mondat!')
+            elif data[0] == 'BETUBEKERES':
+                redraw(act_state)
+                while True:
+                    print('Hibás betűk: ', data[1])
+                    print('Szó:', data[2])
+                    print('Adj meg egy betűt!')
+                    letter = input('> ')
+                    letter = letter.strip().upper()
+                    if is_valid_letter(letter, data):
+                        s.sendall(letter.encode())
+                        break
+                    else:
+                        redraw(act_state)
+                        print('Nem érvényes betű!')
+                        
+
+
+os.system(clearcmd)
+while (True):
+    print(logo)
+    print(menu)
+    in_num = input("\n\tPlease make a selection from: 1, 2, 3: ")
+    if len(in_num) == 1 and in_num[0].isdigit():
+        if in_num == "1":
+            start_game()
+        elif in_num == "2":
+            os.system(clearcmd)
+            print(developers)
+        elif in_num == "3":
+            break
+        else:
+            os.system(clearcmd)
+            print("Nincs ilyen lehetőség!")
